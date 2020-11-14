6d0ee7e44e718602b9df77d2c442df0201df3746
gabriel-avram-selab <gabrielavram1234@gmail.com>
2020-11-14 19:58:08 +0200

    Adaugare numar de telefon furnizor
    Signed-off-by: gabriel-avram-selab <gabrielavram1234@gmail.com>

--- src/furnizori.java
+++ src/furnizori.java
@@ -2,10 +2,11 @@ public class furnizori {
     String nume_furnizor;
     String adress;
     String country;
-
-    public furnizori(String nume_furnizor, String adress, String country) {
+    String numar_telefon;
+    public furnizori(String nume_furnizor, String adress, String country, String nr_tel) {
         this.nume_furnizor = nume_furnizor;
         this.adress = adress;
         this.country = country;
+        this.numar_telefon=nr_tel;
     }
 }
