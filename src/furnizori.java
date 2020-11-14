64161f4b752162ab5b010286db01de23745f39b1
gabriel-avram-selab <gabrielavram1234@gmail.com>
2020-11-14 19:56:20 +0200

    Adaugare clasa operatii si descriere furnizori
    Signed-off-by: gabriel-avram-selab <gabrielavram1234@gmail.com>

--- src/furnizori.java
+++ src/furnizori.java
@@ -1,4 +1,11 @@
 public class furnizori {
     String nume_furnizor;
-    
+    String adress;
+    String country;
+
+    public furnizori(String nume_furnizor, String adress, String country) {
+        this.nume_furnizor = nume_furnizor;
+        this.adress = adress;
+        this.country = country;
+    }
 }
